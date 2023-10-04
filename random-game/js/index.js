a57a18e8df6d45d0296110edf91bb9ac81a5232f
LudmilaSokol <sluda999@yandex.ru>
2023-10-04 17:15:27 +0500

    feat: add food withdrawal

--- random-game/js/index.js
+++ random-game/js/index.js
@@ -2,21 +2,22 @@
 const canvas = document.querySelector('.canvas');
 const ctx = canvas.getContext('2d');
 //кол-во колонок
-let cellCountX = 30;
+let cellCountX = 20;
 //кол-во строк
-let cellCountY = 20;
+let cellCountY = 14;
 //размер ячейки
 let cellSize = (canvas.width) / cellCountX;
+//высота изображения
 canvas.height = cellCountY * cellSize;
-console.log (cellSize);
 //цвет линий
 ctx.strokeStyle = '#957474';
 //ширина линий
 // ctx.lineWidth = 1;
 //цвет заливки
 ctx.fillStyle = '#cb9898';
-
-
+//создаем объект еды
+let foodImg = new Image();
+foodImg.src = "assets/svg/food_1.svg";
 
 
 //рисуем элементы игры
@@ -25,8 +26,10 @@ function draw() {
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   //рисуем ячейки игрового поля
 	drawCells();
+  drawFood();
 }
-draw()
+//вызов функции с указанной периодичностью
+let game = setInterval(draw, 100);
 
 // рисуем ячейки игрового поля
 function drawCells() {
@@ -35,3 +38,9 @@ function drawCells() {
 			// рисуем ячейку
       ctx.strokeRect(i * cellSize, j * cellSize, cellSize, cellSize);
 }
+// рисуем еду
+function drawFood() {
+  //разместить изображение в клетке случайным образом
+    ctx.drawImage(foodImg, 2, 2);
+}
+
