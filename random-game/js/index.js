0527da8fa7dc6f42b1e5072a02618a4ccd0846c7
LudmilaSokol <sluda999@yandex.ru>
2023-10-04 19:44:48 +0500

    feat: add snake head display

--- random-game/js/index.js
+++ random-game/js/index.js
@@ -1,4 +1,5 @@
 /* переменные и константы */
+
 const canvas = document.querySelector('.canvas');
 const ctx = canvas.getContext('2d');
 //кол-во колонок
@@ -24,32 +25,52 @@ let food = {
   x : Math.floor((Math.random() * cellCountX)) * cellSize,
   y : Math.floor((Math.random() * cellCountY)) * cellSize
 }
+//массив для элемента змея
+let snake = [];
+//голова змейки
+snake[0] = {
+  x : Math.floor((cellCountX * cellSize) / 2),
+  y : Math.floor((cellCountY * cellSize) / 2)
+}
 
 //закрашиваем игровое поле
-ctx.fillRect(0, 0, canvas.width, canvas.height);
+ctx.fillRect (0, 0, canvas.width, canvas.height);
 //рисуем ячейки игрового поля
-drawCells();
+drawCells ();
 
 
 //рисуем элементы игры
-function draw() {
+function draw () {
   //еда
-  drawFood();
+  drawFood ();
+
+  //змейка
+  drawSnake ();
 }
 
 //вызов функции с указанной периодичностью
 let game = setInterval(draw, 100);
 
 // рисуем ячейки игрового поля
-function drawCells() {
+function drawCells () {
 	for (var i = 0; i < cellCountX; i++)
 		for (var j = 0; j < cellCountY; j++)
 			// рисуем ячейку
       ctx.strokeRect(i * cellSize, j * cellSize, cellSize, cellSize);
 }
 // рисуем еду
-function drawFood() {
+function drawFood () {
   //разместить изображение в клетке случайным образом
     ctx.drawImage(foodImg, food.x + 2, food.y + 3);
+}
+
+//рисуем змейку
+function drawSnake () {
+  for (let i = 0; i < snake.length; i++) {
+    //цвет заливки
+    ctx.fillStyle = 'red';
+    //закрашиваем звенья змейки
+    ctx.fillRect(snake[i].x, snake[i].y, cellSize, cellSize);
+  }
 }
 
